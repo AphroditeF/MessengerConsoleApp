3acae640491719b110fec426c3a9c98b0ab29c17
@AphroditeF <a.flora@outlook.com>
2018-04-24 22:47:22 +0300

    trash files... (history)

--- PostConsoleApplication/Post.cs
+++ PostConsoleApplication/Post.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PostConsoleApplication
 {
@@ -14,14 +15,17 @@ namespace PostConsoleApplication
         public string Message { get; set; }
         public Date Date { get; set; }
         
-       
+       public string SenderUserName { get; set; }
+        public string ReceiverUserName { get; set; }
         public Condition Condition { get; set; }
 
 
         public string SenderUsername { get; set; }
+        [ForeignKey("SenderUserName")]//
         public virtual User Sender { get; set; }
 
         public string ReceiverUsername { get; set; }
+        [ForeignKey("ReceiverUserName")]
         public virtual User Receiver { get; set; }
 
     }
